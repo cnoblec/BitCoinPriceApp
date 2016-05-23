f948b902e46dc849405924a82024ec18033a592d
rsgc-noblecurveira-c <student@macbook-pro-noble-curviera.local>
2016-05-23 17:08:17 -0400

    Initial Commit

---
+++ BitCoinPriceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitCoinPriceApp
+//
+//  Created by Carlos Noble Curveira on 2016-05-23.
+//  Copyright © 2016 Carlos Noble Curveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
